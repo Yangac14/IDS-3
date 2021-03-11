9b4769a1ebfba8669ae66d2fc57c2d9c0e51f469
TimeTyrant42 <alexklugiewicz@gmail.com>
2021-03-10 21:43:18 -0600

    basic File Integrity and hashing

--- FileIntegrity.py
+++ FileIntegrity.py
@@ -1,12 +1,41 @@
+import os
 
+import pandas as pd
+import hashlib
 
-def specifyCriticalFile(filename=None):
-    pass
-    # adds file to saved csv list or database of files to check
-    # an sql database or alike is probably outside of our immediate scope
+# adds file location to saved csv list to check
+def specifyCriticalFile(fileloc=None):
+    if fileloc is None:
+        # open ui to find file
+        pass
 
+    integrityList = pd.read_csv(os.getcwd()+"/Data/integrityList.csv")
+    integrityList.append(fileloc)
+    integrityList.to_csv(os.getcwd()+"/Data/integrityList.csv")
+
+# runs through the database of files, computing and storing their hashes
+# code pretty much ripped from https://www.pythoncentral.io/hashing-files-with-python/
 def initializeHashTable():
-    pass
-    # runs through the database of files, computing and storing their hashes
+    BLOCKSIZE = 65536
+
+    integrityList = pd.read_csv(os.getcwd()+"/Data/integrityList.csv")
+    hashes = []
+    for fileloc in integrityList:
+        hasher = hashlib.sha3_512()
+        with open(fileloc, 'rb') as afile:
+            buf = afile.read(BLOCKSIZE)
+            while len(buf) > 0:
+                hasher.update(buf)
+                buf = afile.read(BLOCKSIZE)
+        hashes.append(hasher.hexdigest())
+
+    hashes = pd.DataFrame(hashes)
+    hashes.to_csv(os.getcwd()+"/Data/hashTable.csv")
+    # probably could add a timestamp to this
+    # also should add a universal file indexer and use that for indexing,
+    # as otherwise changing the order of integrityList messes up parity with hashTable
+
 
 # def checkHashTable():
+#     pass
+#     # doesn't need to be done yet
